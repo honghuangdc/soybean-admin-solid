96fb570e446b6c9f5d16d67a0f9c6cd3caf45467
Soybean <honghuangdc@gmail.com>
2022-08-12 00:36:46 +0800

    chore(projects): import @hope-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,74 @@
-import { Suspense, lazy } from 'solid-js';
+import { Suspense } from 'solid-js';
 import type { Component } from 'solid-js';
+import {
+  HopeProvider,
+  VStack,
+  Center,
+  Text,
+  HStack,
+  Button,
+  Checkbox,
+  Input,
+  Radio,
+  RadioGroup,
+  useColorMode
+} from '@hope-ui/solid';
+import type { HopeThemeConfig } from '@hope-ui/solid';
 import { AppLoading } from '@/components';
-
-const AppContent = lazy(async () => {
-  await new Promise(resolve => setTimeout(resolve, 1000));
-  return import('./Demo');
-});
 
 const App: Component = () => {
   const appTitle = import.meta.env.VITE_APP_TITLE;
 
+  const config: HopeThemeConfig = {
+    initialColorMode: 'system'
+  };
+
   return (
     <Suspense fallback={<AppLoading title={appTitle} />}>
-      <AppContent />
+      <HopeProvider config={config}>
+        <VStack spacing="$12" padding="$12">
+          <Center>
+            <Text color="$primary9" size="2xl">
+              SoybeanAdmin
+            </Text>
+          </Center>
+          <ColorModeSwitcher />
+          <HStack spacing="$4" justifyContent="center">
+            <Button colorScheme="primary">Button</Button>
+            <Button colorScheme="accent">Button</Button>
+            <Button colorScheme="neutral">Button</Button>
+            <Button colorScheme="success">Button</Button>
+            <Button colorScheme="info">Button</Button>
+            <Button colorScheme="warning">Button</Button>
+            <Button colorScheme="danger">Button</Button>
+          </HStack>
+          <HStack spacing="$4">
+            <Checkbox defaultChecked colorScheme="primary" />
+            <Checkbox defaultChecked colorScheme="accent" />
+            <Checkbox defaultChecked colorScheme="neutral" />
+            <Checkbox defaultChecked colorScheme="success" />
+            <Checkbox defaultChecked colorScheme="info" />
+            <Checkbox defaultChecked colorScheme="warning" />
+            <Checkbox defaultChecked colorScheme="danger" />
+          </HStack>
+          <Input placeholder="SoybeanAdmin" />
+          <RadioGroup defaultValue="1">
+            <HStack spacing="$4">
+              <Radio value="1">First</Radio>
+              <Radio value="2">Second</Radio>
+              <Radio value="3">Third</Radio>
+            </HStack>
+          </RadioGroup>
+        </VStack>
+      </HopeProvider>
     </Suspense>
   );
 };
+
+function ColorModeSwitcher() {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return <Button onClick={toggleColorMode}>Toggle {colorMode() === 'light' ? 'dark' : 'light'}</Button>;
+}
 
 export default App;
